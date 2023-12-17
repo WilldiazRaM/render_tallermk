e87f1b0060bc41dde8ab174e572c8330fe7bc1ff
WilldiazRaM <wi.diaz@duocuc.cl>
2023-12-17 18:03:17 -0300

    se pasa sql a postgresql

--- tallermk/settings.py
+++ tallermk/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,10 +94,7 @@ WSGI_APPLICATION = 'tallermk.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL", "postgres://inventorymine_user:RuigIkU7Fyk6kkYW6RjLA69l1tyUdl0j@dpg-cl4pbgil7jac73c53it0-a.oregon-postgres.render.com/inventorymine"))
 }
 
 
